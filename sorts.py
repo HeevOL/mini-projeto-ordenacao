3ccd6d6406d2f5a8b6d8a40f486130a7975e41dd
adriantorress <adriantorrescv@gmail.com>
2022-11-17 17:44:31 -0300

    função half para pegar a metade da lista

--- sorts.py
+++ sorts.py
@@ -1,15 +1,27 @@
+def half(lista):
+    esquerda = []
+    direita = []
+    meio = len(lista)//2
+    for x in range(len(lista)):
+        if x < meio:
+            esquerda.append(lista[x])
+        else:
+            direita.append(lista[x])
+    return esquerda, direita
+
 # Selection apenas com o maior
 
+
 def selectionSort1(uma_lista):
-    for posicao_verificada in range(len(uma_lista)-1,0,-1):
+    for posicao_verificada in range(len(uma_lista)-1, 0, -1):
         posicao_maior = 0
-        for posicao in range(1,posicao_verificada+1):
-            if uma_lista[posicao]>uma_lista[posicao_maior]:
+        for posicao in range(1, posicao_verificada+1):
+            if uma_lista[posicao] > uma_lista[posicao_maior]:
                 posicao_maior = posicao
         temp = uma_lista[posicao_verificada]
         uma_lista[posicao_verificada] = uma_lista[posicao_maior]
         uma_lista[posicao_maior] = temp
-        
+
 
 # Selection com maior e menor
 
@@ -22,13 +34,13 @@ def selectionSort(uma_lista):
         for posicao in range(limite_esquerdo, limite_direito+1):
             if uma_lista[posicao] >= uma_lista[posicao_maior]:
                 posicao_maior = posicao
-                
+
             if uma_lista[posicao] <= uma_lista[posicao_menor]:
                 posicao_menor = posicao
 
         if limite_direito == posicao_menor and limite_esquerdo == posicao_maior:
             uma_lista[limite_direito], uma_lista[limite_esquerdo] = uma_lista[limite_esquerdo], uma_lista[limite_direito]
-            
+
         else:
             temp_maior = uma_lista[limite_direito]
             temp_menor = uma_lista[limite_esquerdo]
@@ -55,34 +67,32 @@ def selectionSort(uma_lista):
 # MergeSort com slice
 
 def mergeSort(uma_lista):
-    if len(uma_lista)>1:
-        meio = len(uma_lista)//2
-        esquerda = uma_lista[:meio]
-        direita = uma_lista[meio:]
+    if len(uma_lista) > 1:
+        esquerda, direita = half(uma_lista)
         mergeSort(esquerda)
         mergeSort(direita)
-        i=0
-        j=0
-        k=0
+        i = 0
+        j = 0
+        k = 0
 
         while i < len(esquerda) and j < len(direita):
             if esquerda[i] < direita[j]:
-                uma_lista[k]=esquerda[i]
-                i=i+1
+                uma_lista[k] = esquerda[i]
+                i = i+1
             else:
-                uma_lista[k]=direita[j]
-                j=j+1
-            k=k+1
+                uma_lista[k] = direita[j]
+                j = j+1
+            k = k+1
 
         while i < len(esquerda):
-            uma_lista[k]=esquerda[i]
-            i=i+1
-            k=k+1
+            uma_lista[k] = esquerda[i]
+            i = i+1
+            k = k+1
 
         while j < len(direita):
-            uma_lista[k]=direita[j]
-            j=j+1
-            k=k+1
+            uma_lista[k] = direita[j]
+            j = j+1
+            k = k+1
 
     return uma_lista
 
@@ -90,34 +100,34 @@ def mergeSort(uma_lista):
 # Merge + Selection
 
 def hibridSort(uma_lista):
-    if len(uma_lista)>128:
+    if len(uma_lista) > 128:
         meio = len(uma_lista)//2
         esquerda = uma_lista[:meio]
         direita = uma_lista[meio:]
         hibridSort(esquerda)
         hibridSort(direita)
-        i=0
-        j=0
-        k=0
+        i = 0
+        j = 0
+        k = 0
 
         while i < len(esquerda) and j < len(direita):
             if esquerda[i] < direita[j]:
-                uma_lista[k]=esquerda[i]
-                i=i+1
+                uma_lista[k] = esquerda[i]
+                i = i+1
             else:
-                uma_lista[k]=direita[j]
-                j=j+1
-            k=k+1
+                uma_lista[k] = direita[j]
+                j = j+1
+            k = k+1
 
         while i < len(esquerda):
-            uma_lista[k]=esquerda[i]
-            i=i+1
-            k=k+1
+            uma_lista[k] = esquerda[i]
+            i = i+1
+            k = k+1
 
         while j < len(direita):
-            uma_lista[k]=direita[j]
-            j=j+1
-            k=k+1
+            uma_lista[k] = direita[j]
+            j = j+1
+            k = k+1
 
     else:
         limite_esquerdo = 0
@@ -128,7 +138,7 @@ def hibridSort(uma_lista):
             for posicao in range(limite_esquerdo, limite_direito+1):
                 if uma_lista[posicao] >= uma_lista[posicao_maior]:
                     posicao_maior = posicao
-                    
+
                 if uma_lista[posicao] <= uma_lista[posicao_menor]:
                     posicao_menor = posicao
 
@@ -158,5 +168,5 @@ def hibridSort(uma_lista):
 
 
 if __name__ == "__main__":
-    lista = [2,1,3,8,5,6,7,9,4,10,2,1.2,3,8,5,6,7,9,4,10]
+    lista = [2, 1, 3, 8, 5, 6, 7, 9, 4, 10, 2, 1.2, 3, 8, 5, 6, 7, 9, 4, 10]
     print(selectionSort(lista))
